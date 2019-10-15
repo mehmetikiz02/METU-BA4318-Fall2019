2fec15d872bcad713dffe1f8f07e0ac6d8b2a703
mehmetikiz02 <56003947+mehmetikiz02@users.noreply.github.com>
2019-10-15 15:28:32 +0300

    HW 3

---
+++ Homework 3 by Mehmet İKİZ.py
@@ -0,0 +1,15 @@
+age = int(input("What is your age? " ))
+if 19 < age :
+    print("You are an adult")
+    
+if 10 < age <= 19 :
+    print("You are an adolescent")
+    
+if 1 <= age <= 10:
+    print("You are a child")
+    
+if age < 1 :
+    print("You are an infant")
+    
+
+          
